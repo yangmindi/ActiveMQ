10f96c692e5fc88917d8f09704eebb47df267bc8
ymd <1946334058@qq.com>
2019-11-09 18:49:47 +0800

    produce scheduled produce

--- boot_mq_produce/src/main/java/com/atguigu/boot/activemq/produce/Queue_Produce.java
+++ boot_mq_produce/src/main/java/com/atguigu/boot/activemq/produce/Queue_Produce.java
@@ -3,6 +3,7 @@ package com.atguigu.boot.activemq.produce;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -18,4 +19,12 @@ public class Queue_Produce {
     public void produceMsg(){
         jmsMessagingTemplate.convertAndSend(queue,"******：" + UUID.randomUUID().toString().substring(0,6));
     }
+
+    //间隔时间3秒钟定投
+    @Scheduled(fixedDelay = 3000)
+    public void produceMsgScheduled(){
+        jmsMessagingTemplate.convertAndSend(queue,"******Scheduled:"+UUID.randomUUID().toString().substring(0,6));
+        System.out.println("*****produceMsgScheduled send ok");
+    }
+
 }
